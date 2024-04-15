9c1297cc36c4bd3fce410669baeb7c765ee5730a
razrabot4ik123 <121711882+razrabot4ik123@users.noreply.github.com>
2024-04-15 20:35:55 +0700

    Add files via upload

--- js-89/main.js
+++ js-89/main.js
@@ -1,74 +1,26 @@
-// const button = document.querySelector('.btn')
+// const btn = document.querySelector('.btn')
+// const text = document.querySelector('.text')
 
-// button.addEventListener('click', () => {
-//     console.log('клик сработал')
+
+// добавить класс
+// btn.addEventListener('click', () => {
+//     text.classList.add('red')
 // })
 
-// function scrolling() {
-//     console.log('scroll')
-// }
+// посмотреть
+// console.log(text.classList)
 
-// document.addEventListener('scroll', scrolling)
-
-
-
-// document.addEventListener('scroll', () => {
-//     console.log('scroll')
+// btn.addEventListener('click', () => {
+//     text.classList.toggle('red')
+//     text.classList.toggle('move')
+//     text.classList.remove('abrakadabra')
 // })
 
-
-
-
-
-
-// const button = document.querySelector('.btn')
-// let btnIsRed = false
-
-// button.addEventListener('click', () => {
-// button.style.backgroundColor = 'red'
-
-//     if (btnIsRed === true) {
-//         button.style.backgroundColor = 'blue'
-//         btnIsRed = false
+// btn.addEventListener('click', () => {
+//     text.classList.toggle('move')
+//     if (text.classList.contains('move')) {
+//         text.textContent = 'я двигаюсь'
 //     } else {
-//         button.style.backgroundColor = 'red'
-//         btnIsRed = true
+//         text.innerText = 'text'
 //     }
 // })
-
-// function scrolling() {
-//     console.log('scroll')
-// }
-
-// document.addEventListener('scroll', scrolling)
-
-
-
-
-
-
-
-
-
-
-
-
-// Событие submit происходит, когда пользователь отправляет форму, нажимая на кнопку отправки (обычно это кнопка с типом "submit") 
-// или нажимая Enter на клавиатуре, находясь в текстовом поле формы.
-
-// Когда событие submit возникает, браузер собирает данные из всех элементов формы и отправляет их на сервер для обработки. После 
-// этого обычно происходит переход на другую страницу или показ сообщения об успешной отправке данных.
-
-
-
-
-
-
-// function logSubmit(event) {
-//     log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
-//     event.preventDefault();
-// }
-
-// const form = document.getElementById("form");
-// const log = document.getElementById("log");
-// form.addEventListener("submit", logSubmit);
---
+++ js-90/main.js
@@ -0,0 +1,35 @@
+// const btns = document.querySelectorAll('.btn')
+
+// btns.forEach((btn, index) => {
+//     btn.addEventListener('click', () => {
+//         console.log(index + 1)
+//     })
+// })
+
+
+// const btn = document.querySelector('.btn')
+
+// btn.addEventListener('click', (event) => {
+//     console.log(event.target)
+// })
+
+
+
+// btn.addEventListener('click', (event) => {
+//     event.target.classList.toggle('red')
+// })
+
+// btn.addEventListener('click', (event) => {
+//     event.currentTarget.classList.toggle('red')
+// })
+
+
+
+
+
+
+// Есть два способа отменить действие браузера:
+// В следующем примере при клике по ссылке переход не произойдёт:
+// <a href="/" onclick="return false">Нажми здесь</a>
+// или
+// <a href="/" onclick="event.preventDefault()">здесь</a>
