45a5a4f9b65080b330b4969423336aa763908cc4
razrabot4ik123 <121711882+razrabot4ik123@users.noreply.github.com>
2024-04-23 20:12:50 +0700

    Update main.js

--- html-94-126/wake-bake/js/main.js
+++ html-94-126/wake-bake/js/main.js
@@ -55,30 +55,30 @@
 
 // тут при помощи чата gpt обернул свой код в функцию
 
-// (function () {
-//     const burgerWinodw = document.querySelector('.burger-icon');
-//     const openBurgerMenu = document.body;
-//     const closeWitchLinksBurger = document.querySelectorAll('.nav__link');
-//     let burgerIsopen = false;
-//     burgerWinodw.addEventListener('click', () => {
-//         if (!burgerIsopen) {
-//             openBurgerMenu.classList.add('body--opened-menu');
-//             burgerIsopen = true;
-//         } else {
-//             openBurgerMenu.classList.remove('body--opened-menu');
-//             burgerIsopen = false;
-//         }
-//     });
+(function () {
+    const burgerWinodw = document.querySelector('.burger-icon');
+    const openBurgerMenu = document.body;
+    const closeWitchLinksBurger = document.querySelectorAll('.nav__link');
+    let burgerIsopen = false;
+    burgerWinodw.addEventListener('click', () => {
+        if (!burgerIsopen) {
+            openBurgerMenu.classList.add('body--opened-menu');
+            burgerIsopen = true;
+        } else {
+            openBurgerMenu.classList.remove('body--opened-menu');
+            burgerIsopen = false;
+        }
+    });
 
-//     closeWitchLinksBurger.forEach((nav__link) => {
-//         nav__link.addEventListener('click', () => {
-//             if (burgerIsopen) {
-//                 openBurgerMenu.classList.remove('body--opened-menu');
-//                 burgerIsopen = false;
-//             }
-//         });
-//     });
-// })();
+    closeWitchLinksBurger.forEach((nav__link) => {
+        nav__link.addEventListener('click', () => {
+            if (burgerIsopen) {
+                openBurgerMenu.classList.remove('body--opened-menu');
+                burgerIsopen = false;
+            }
+        });
+    });
+})();
 
 // тут при помощи чата gpt обернул свой код в самовызывающую функцию
 // вроде как все три мои способа выполняют условия, которые были заданы, как писал код преподователь я не смотрел, появились дела, тайминг где остановился - (102 урок, 08:20), и посмотрев дальше увидел что преподователь начал писать код в самовызывающей функции, и обернул через gpt
